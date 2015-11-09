546f98b4c701cd910d0fb1ea7bf8b948a12e145c
PrestonGardner <Preston.A.Gardner@hotmail.com>
2015-11-08 21:26:26 -0700

    added row fields

--- project/models.py
+++ project/models.py
@@ -14,6 +14,10 @@ class Cbom(db.Model):
 class CbomRow(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     cbom_id = db.Column(db.Integer, db.ForeignKey('cbom.id'))
+    man_part_num = db.Column(db.String(50))
+    int_part_num = db.Column(db.String(50))
+    unit_price = db.Column(db.Numeric)
+    quantity = db.Column(db.Integer)
     
 
 class User(db.Model):
