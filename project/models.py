c6965138e0b31cf5d7ece80b971f4a51c2a34a9a
PrestonGardner <Preston.A.Gardner@hotmail.com>
2015-11-08 21:17:32 -0700

    added row class

--- project/models.py
+++ project/models.py
@@ -9,6 +9,12 @@ from project import db
 class Cbom(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(265), unique=True, nullable=False)
+    rows = db.relationship('CbomRow',backref='cbom',lazy='dynamic')
+
+class CbomRow(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cbom_id = db.Column(db.Integer, db.ForeignKey('cbom.id'))
+    
 
 class User(db.Model):
 
