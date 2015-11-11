c13c9ba793784d3792ef4c90d0e884bdafb9a3f9
FalseShepherd <maxkolb741@gmail.com>
2015-11-11 11:17:32 -0500

    october help, idk how to html

---
+++ project/cbom/Estimate.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from os import path
+from project.models import Cbom, CbomRow
+from project import db
+
+# def Estimate(BOM)
+#     df = pd.read_excel(BOM)
+#     for row in df.iterrows():
+#
+#
+#     df["Price"]= CBOM.row.query["Price"]
--- project/cbom/views.py
+++ project/cbom/views.py
@@ -9,10 +9,11 @@ from flask import render_template, Blueprint
 from flask_login import login_required
 from flask import flash, redirect, url_for, session, request
 from .forms import CbomUploadForm, CbomSearchForm
-from project.models import Cbom
+from project.models import Cbom, CbomRow
 from sqlalchemy.exc import IntegrityError
 from io import BytesIO
 from project.cbom.import_cbom import import_cbom
+#from project.cbom.Estimate import Estimate
 
 
 
@@ -39,6 +40,19 @@ def upload():
         #     flash('Error uploading file')
     return render_template('cbom/upload.html', form=form)
 
+@main_blueprint.route('/Estimate/', methods=('GET', 'POST'))
+def estimate():
+    form = CbomUploadForm()
+    if form.validate_on_submit():
+         try:
+            BOM = request.files[form.file.data.name].read()
+            filename = request.files[form.file.data.name].filename
+            #Estimate(BOM)
+            flash('Uploaded file ', filename)
+         except:
+             flash('Error uploading file')
+    return render_template('cbom/upload.html', form=form)
+
 @main_blueprint.route('/')
 def home():
     return render_template('cbom/home.html')
@@ -56,7 +70,12 @@ def search_cbom():
         ).all()
         # if there are matching cboms, show user list of them to select from
         if len(matching_cboms) > 0:
-            return render_template('cbom/list_view.html', cboms=matching_cboms)
+            return render_template('cbom/list_view.html', cboms=matching_cboms, str=str)
         else:
             flash('No matching CBOMs found')
     return render_template("cbom/search_cbom.html", form=form)
+
+@main_blueprint.route("/view_cbom_<id>/")
+def view_cbom(id):
+    CBOM = Cbom.query.filter(id)
+    return render_template("cbom/view_cbom/", CBOM = CBOM)
