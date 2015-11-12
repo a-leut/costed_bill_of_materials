c8ba3604d4339956a9b980c8bc049dd127aa12dc
doop <alex.leutenegger@gmail.com>
2015-11-12 02:40:56 -0700

    fixed grid view and fixed file downloading as csv

--- project/cbom/views.py
+++ project/cbom/views.py
@@ -5,16 +5,14 @@
 #### imports ####
 #################
 
-from flask import render_template, Blueprint
-from flask_login import login_required
-from flask import flash, redirect, url_for, session, request
+from flask import render_template, Blueprint, send_file
+from flask import flash, request
 from .forms import CbomUploadForm, CbomSearchForm
 from project.models import Cbom, CbomRow
-import flask_excel as excel
-from io import BytesIO
+from io import BytesIO, StringIO
 from project.cbom.import_cbom import import_cbom
 from project.cbom.estimate import estimate_bom
-
+import pandas as pd
 
 
 ################
@@ -31,13 +29,10 @@ main_blueprint = Blueprint('cbom', __name__,)
 def upload():
     form = CbomUploadForm()
     if form.validate_on_submit():
-        # try:
         file = request.files[form.file.data.name].read()
         filename = request.files[form.file.data.name].filename
         import_cbom(BytesIO(file), filename)
         flash('Uploaded file ', filename)
-        # except:
-        #     flash('Error uploading file')
     return render_template('cbom/upload.html', form=form)
 
 @main_blueprint.route('/estimate/', methods=('GET', 'POST'))
@@ -46,18 +41,23 @@ def estimate():
     if form.validate_on_submit():
         bom = request.files[form.file.data.name].read()
         filename = request.files[form.file.data.name].filename
-        output = estimate_bom(BytesIO(bom))
-        return excel.make_response_from_dict(output.to_dict(), 'csv')
+        # estimate the cost of the cbom
+        df = estimate_bom(BytesIO(bom))
+        # create excel file in memory and return to user
+        io = StringIO()
+        writer = pd.ExcelWriter('not_used.xlsx')
+        writer.book.filename = io
+        df.to_excel(writer)
+        writer.close()
+        return send_file(io,
+                         as_attachment=True,
+                         attachment_filename="estimated_" + filename)
 
     return render_template('cbom/estimate.html', form=form)
 
 @main_blueprint.route('/')
 def home():
     return render_template('cbom/home.html')
-
-@main_blueprint.route("/cbom/")
-def cbom():
-    return render_template("cbom/about.html")
 
 @main_blueprint.route("/search_cbom/", methods=['GET', 'POST'])
 def search_cbom():
@@ -75,6 +75,6 @@ def search_cbom():
 
 @main_blueprint.route("/view_cbom_<id>/")
 def view_cbom(id):
-    CBOM = Cbom.query.filter(Cbom.id == cbom.id).first()
-    cbom_rows = CbomRow.query.filter(CbomRow.cbom_id == cbom_id).all()
-    return render_template("cbom/view_cbom/", CBOM = CBOM, rows = cbom_rows)
+    CBOM = Cbom.query.filter(Cbom.id == id).first()
+    cbom_rows = CbomRow.query.filter(CbomRow.cbom_id == id).all()
+    return render_template("cbom/view_cbom.html/", CBOM=CBOM, rows=cbom_rows)
