aec4e6b3e1ac0e957dd2135a750ad23e1b01b5e4
doop <alex.leutenegger@gmail.com>
2015-11-12 02:00:42 -0700

    got it returning excel file on estimate

--- project/cbom/Estimate.py
+++ project/cbom/Estimate.py
@@ -1,13 +1,13 @@
 import pandas as pd
-from os import path
-from project.models import Cbom, CbomRow
-from project import db
+import numpy as np
+from project.models import CbomRow
 
-def Estimate(BOM)
-    df = pd.read_excel(BOM)
-    for row in df.iterrows():
-        if(row[1]["Manufacturer Part"] != None):
-                row['Unit Price'] = CbomRow.query.filter(row[1]["Manufacturer Part"]).order_by(CbomRow.upload_date).unit_price
-        elif(row[1]["Customer Part Number"] != None):
-                row['Unit Price'] = CbomRow.query.filter(row[1]["Customer Part Number"]).order_by(CbomRow.upload_date).unit_price
-    #somehow set this to the user desktop.
+def estimate_bom(bom):
+    df = pd.read_excel(bom)
+    df['Unit Price'] = np.nan
+    # for row in df.iterrows():
+    #     if(row[1]["Manufacturer Part"] != None):
+    #             df.loc[1]['Unit Price'] = CbomRow.query.filter(row[1]["Manufacturer Part"]).order_by(CbomRow.upload_date).first().unit_price
+    #     elif(row[1]["Customer Part Number"] != None):
+    #             df.loc[1]row['Unit Price'] = CbomRow.query.filter(row[1]["Customer Part Number"]).order_by(CbomRow.upload_date).first().unit_price
+    return df
