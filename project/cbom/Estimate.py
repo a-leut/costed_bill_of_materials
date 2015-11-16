f737ac6c87be66f1d70f849c59cbc7594d8c27a3
FalseShepherd <maxkolb741@gmail.com>
2015-11-15 19:54:47 -0500

    estimator working

--- project/cbom/Estimate.py
+++ project/cbom/Estimate.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import numpy as np
+from project import db
 from project.models import CbomRow
 
 def estimate_bom(bom):
     df = pd.read_excel(bom)
     print(df)
-    # for row in df.iterrows():
-    #     if(row[1]["Manufacturer Part"] != None):
-    #             df.loc[1]['Unit Price'] = CbomRow.query.filter(row[1]["Manufacturer Part"]).order_by(CbomRow.upload_date).first().unit_price
-    #     elif(row[1]["Customer Part Number"] != None):
-    #             df.loc[1]row['Unit Price'] = CbomRow.query.filter(row[1]["Customer Part Number"]).order_by(CbomRow.upload_date).first().unit_price
+    df["Price"]= df["Customer Part"].apply(cpn_lookup)
+    df["Price"]= df["Manufacturer Part"].apply(mpn_lookup)
     return df
+def mpn_lookup(mpn):
+    return CbomRow.query.filter(CbomRow.mpn == mpn).order_by(CbomRow.upload_date).first().unit_price
+
+
+def cpn_lookup(cpn):
+    return CbomRow.query.filter(CbomRow.cpn == cpn).order_by(CbomRow.upload_date).first().unit_price
