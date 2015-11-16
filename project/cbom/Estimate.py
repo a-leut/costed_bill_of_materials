96c388d51d4a40f7861a33285c435c3e7fb68e27
doop <alex.leutenegger@gmail.com>
2015-11-15 20:00:38 -0700

    added pricing

--- project/cbom/Estimate.py
+++ project/cbom/Estimate.py
@@ -6,12 +6,20 @@ from project.models import CbomRow
 def estimate_bom(bom):
     df = pd.read_excel(bom)
     print(df)
-    df["Price"]= df["Customer Part"].apply(cpn_lookup)
+    df["Price"]= df["Customer Part Number"].apply(cpn_lookup)
     df["Price"]= df["Manufacturer Part"].apply(mpn_lookup)
     return df
-def mpn_lookup(mpn):
-    return CbomRow.query.filter(CbomRow.mpn == mpn).order_by(CbomRow.upload_date).first().unit_price
 
+def mpn_lookup(mpn):
+    rec = CbomRow.query.filter(CbomRow.mpn == mpn).order_by(CbomRow.upload_date).first()
+    if rec:
+        return rec.unit_price
+    else:
+        return 0
 
 def cpn_lookup(cpn):
-    return CbomRow.query.filter(CbomRow.cpn == cpn).order_by(CbomRow.upload_date).first().unit_price
+    rec = CbomRow.query.filter(CbomRow.cpn == cpn).order_by(CbomRow.upload_date).first()
+    if rec:
+        return rec.unit_price
+    else:
+        return 0
