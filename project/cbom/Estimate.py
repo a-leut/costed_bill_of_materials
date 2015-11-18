6b9b87d0353e74d219ddfa7122fd46d9a97b2dbe
FalseShepherd <maxkolb741@gmail.com>
2015-11-17 21:43:22 -0500

    MOQ Update

--- project/cbom/Estimate.py
+++ project/cbom/Estimate.py
@@ -6,6 +6,7 @@ def estimate_bom(bom):
     df = pd.read_excel(bom)
     print(df)
     df["Unit Price"] = df.apply(estimate_row, axis=1)
+    df["MOQ"] = df.apply(estimate_row_MOQ, axis=1)
     return df
 
 def estimate_row(row):
@@ -26,5 +27,25 @@ def estimate_row(row):
             order_by(CbomRow.upload_date).first()
         if rec:
             return rec.unit_price
+        else:
+            return 0
+def estimate_row_MOQ(row):
+    """
+    :param row: Row in dataframe with mpn and cpn column
+    :return: Price of record matching on mpn or cpn if it exists
+    """
+    # first lookup by mpn
+    mpn = row["Manufacturer Part"]
+    rec = CbomRow.query.filter(CbomRow.mpn == mpn).\
+        order_by(CbomRow.upload_date).first()
+    if rec:
+        return rec.MOQ
+    else:
+        # otherwise lookup by cpn
+        cpn = row["Customer Part Number"]
+        rec = CbomRow.query.filter(CbomRow.cpn == cpn).\
+            order_by(CbomRow.upload_date).first()
+        if rec:
+            return rec.MOQ
         else:
             return 0
